bc14bc609b36b508d1da2a192de4c6a0a63fd40c
Gulshan Osmonalieva <gulshan.osmonalieva.kk@gmail.com>
2022-05-16 23:58:22 +0600

    finished

---
+++ script3.js
@@ -0,0 +1,5 @@
+//Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let firstName = "Gulshan";
+let userEmail = "gulshan.osmonalieva.kk@gmail.com";
+
+console.log("My name:" + firstName + "  my email:" + userEmail);
